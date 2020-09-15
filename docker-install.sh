b458ded12ec36ede3d82d533b7602516046bc9bf
fenglin <linfeng.fxl@gmail.com>
2020-09-15 14:42:53 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -11,7 +11,7 @@
     	echo "安装docker环境..."
        # curl -sSL https://get.daocloud.io/docker | sh
         echo "安装docker环境...安装完成!"
-    fi
+    #fi
     # 创建公用网络==bridge模式
     #docker network create share_network
 }
